6132aafcd58983c0245838bb37e8aff583a96377
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2018-01-25 15:59:05 -0800

    Blow away MainActivity and start with new approach to checking answers

--- app/src/main/java/com/example/android/beerquiz/MainActivity.java
+++ app/src/main/java/com/example/android/beerquiz/MainActivity.java
@@ -17,151 +17,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /* Initializes the score to 0 */
-    int score = 0;
-
-    /**
-     * Grades question #1
-     * checks that false (answer 1b) is selected
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer1(int score) {
-        RadioButton correct_answer = findViewById(R.id.answer_1b);
-        if (correct_answer.isChecked()) {
-            score += 1;
-        }
-        return score;
-    }
-
-    /**
-     * Grades question #2
-     * Checks that the correct answers, B and C, are checked
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer2(int score) {
-        CheckBox isChecked_2a = findViewById(R.id.answer_2a);
-        boolean answer_2a = isChecked_2a.isChecked();
-        CheckBox isChecked_2b = findViewById(R.id.answer_2b);
-        boolean answer_2b = isChecked_2b.isChecked();
-        CheckBox isChecked_2c = findViewById(R.id.answer_2c);
-        boolean answer_2c = isChecked_2c.isChecked();
-
-        int i = 0;
-
-        if (!answer_2a) {
-            i += 1;
-        }
-        if (answer_2b) {
-            i += 1;
-        }
-        if (answer_2c) {
-            i += 1;
-        }
-        if (i == 3) {
-            score += 1;
-        }
-        return score;
-    }
-
-    /**
-     * Grades question #3
-     * checks that the correct answer ("growler") was entered
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer3(int score) {
-        EditText answer_3 = findViewById(R.id.answer_3);
-        if (answer_3.getText().toString() == "growler") {
-            score += 1;
-        }
-        return score;
-    }
-
-    /**
-     * Grades question #4
-     * checks that answer 4a is selected
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer4(int score) {
-        RadioButton correct_answer = findViewById(R.id.answer_4a);
-        if (correct_answer.isChecked()) {
-            score += 1;
-        }
-        return score;
-    }
-
-    /**
-     * Grades question #5
-     * Checks that the correct answers, A and C, are checked
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer5(int score) {
-        CheckBox isChecked_5a = findViewById(R.id.answer_5a);
-        boolean answer_5a = isChecked_5a.isChecked();
-        CheckBox isChecked_5b = findViewById(R.id.answer_5b);
-        boolean answer_5b = isChecked_5b.isChecked();
-        CheckBox isChecked_5c = findViewById(R.id.answer_5c);
-        boolean answer_5c = isChecked_5c.isChecked();
-
-        int i = 0;
-
-        if (answer_5a) {
-            i += 1;
-        }
-        if (!answer_5b) {
-            i += 1;
-        }
-        if (answer_5c) {
-            i += 1;
-        }
-        if (i == 3) {
-            score += 1;
-        }
-        return score;
-    }
-
-    /**
-     * Grades question #6
-     * checks that the user is a true beer fan
-     *
-     * @param score the current score
-     * @return score is returned after being incremented if answer is correct
-     */
-    private int checkAnswer6(int score) {
-        EditText answer_3 = findViewById(R.id.answer_6);
-        if (answer_3.getText().toString() == "Brewing beer is both more difficult and more rewarding than putting grape juice in a barrel.") {
-            score += 1;
-        }
-        return score;
-    }
-
-
-
-    /**
-     * When Submit Answers button is pressed, runs a check on each answer given and gives the user a toast of their score
-     *
-     *
-     *
-     */
     public void gradeSubmission(View view) {
-        checkAnswer1(score);
-        checkAnswer2(score);
-        checkAnswer3(score);
-        checkAnswer4(score);
-        checkAnswer5(score);
-        checkAnswer6(score);
+//initialize variables for answer views
+        RadioButton question1_RadioButton1 = findViewById(R.id.answer_1a);
+        RadioButton question1_RadioButton2 = findViewById(R.id.answer_1b);
+        CheckBox question2_CheckBox1 = findViewById(R.id.answer_2a);
+        CheckBox question2_CheckBox2 = findViewById(R.id.answer_2b);
+        CheckBox question2_CheckBox3 = findViewById(R.id.answer_2c);
+        RadioButton question3_RadioButton1 = findViewById(R.id.answer_3a);
+        RadioButton question3_RadioButton2 = findViewById(R.id.answer_3b);
+        RadioButton question3_RadioButton3 = findViewById(R.id.answer_3c);
+        CheckBox question4_CheckBox1 = findViewById(R.id.answer_4a);
+        CheckBox question4_CheckBox2 = findViewById(R.id.answer_4b);
+        CheckBox question4_CheckBox3 = findViewById(R.id.answer_4c);
 
-        Toast.makeText(getApplicationContext(), "You scored a " + score + " out of 6", Toast.LENGTH_LONG).show();
+//get the status of the checkboxes
+        boolean yesQuestion1_RadioButton1 = question1_RadioButton1.isChecked();
+        boolean yesQuestion2_CheckBox1 = question2_CheckBox1.isChecked();
+        boolean yesQuestion2_CheckBox2 = question2_CheckBox2.isChecked();
+        boolean yesQuestion2_CheckBox3 = question2_CheckBox3.isChecked();
+        boolean yesQuestion3_RadioButton1 = question3_RadioButton1.isChecked();
+        boolean yesQuestion3_RadioButton2 = question3_RadioButton2.isChecked();
+        boolean yesQuestion3_RadioButton3 = question3_RadioButton3.isChecked();
 
+
+//now grant score based on which value is true
+        int finalGrade = 0;
+
+        if (yesQuestion1_RadioButton1) {
+            finalGrade = finalGrade + 1;
+        }
+
+        Toast.makeText(getApplicationContext(), "You scored a " + finalGrade + " out of 6", Toast.LENGTH_LONG).show();
     }
 
 }
