ed4883117e87b64acbd2129d291fac5a0e65ff9f
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2018-01-29 18:49:27 -0800

    Add logic to check answers for multiple choice questions. Free Response still broken

--- app/src/main/java/com/example/android/beerquiz/MainActivity.java
+++ app/src/main/java/com/example/android/beerquiz/MainActivity.java
@@ -31,21 +31,48 @@ public class MainActivity extends AppCompatActivity {
         CheckBox question4_CheckBox2 = findViewById(R.id.answer_4b);
         CheckBox question4_CheckBox3 = findViewById(R.id.answer_4c);
 
-//get the status of the checkboxes
-        boolean yesQuestion1_RadioButton1 = question1_RadioButton1.isChecked();
+//get the status of the checkboxes or radio buttons
+        boolean yesQuestion1_RadioButton2 = question1_RadioButton2.isChecked();
         boolean yesQuestion2_CheckBox1 = question2_CheckBox1.isChecked();
         boolean yesQuestion2_CheckBox2 = question2_CheckBox2.isChecked();
         boolean yesQuestion2_CheckBox3 = question2_CheckBox3.isChecked();
         boolean yesQuestion3_RadioButton1 = question3_RadioButton1.isChecked();
         boolean yesQuestion3_RadioButton2 = question3_RadioButton2.isChecked();
         boolean yesQuestion3_RadioButton3 = question3_RadioButton3.isChecked();
+        boolean yesQuestion4_CheckBox1 = question4_CheckBox1.isChecked();
+        boolean yesQuestion4_CheckBox2 = question4_CheckBox2.isChecked();
+        boolean yesQuestion4_CheckBox3 = question4_CheckBox3.isChecked();
+
+//get string of each fill in the blank answer
+        EditText answerQuestion5 = findViewById(R.id.answer_5);
+        EditText answerQuestion6 = findViewById(R.id.answer_6);
 
 
 //now grant score based on which value is true
         int finalGrade = 0;
 
-        if (yesQuestion1_RadioButton1) {
-            finalGrade = finalGrade + 1;
+        if (yesQuestion1_RadioButton2) {
+            finalGrade += 1;
+        }
+
+        if(yesQuestion2_CheckBox1 && yesQuestion2_CheckBox2 && yesQuestion2_CheckBox3) {
+            finalGrade += 1;
+        }
+
+        if (yesQuestion3_RadioButton1) {
+            finalGrade += 1;
+        }
+
+        if(!yesQuestion4_CheckBox1 && yesQuestion4_CheckBox2 && !yesQuestion4_CheckBox3) {
+            finalGrade += 1;
+        }
+
+        if (answerQuestion5.getText().toString() == "growler") {
+            finalGrade += 1;
+        }
+
+        if (answerQuestion6.getText().toString() == "yeast") {
+            finalGrade += 1;
         }
 
         Toast.makeText(getApplicationContext(), "You scored a " + finalGrade + " out of 6", Toast.LENGTH_LONG).show();
