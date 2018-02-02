5b8de64d776ebc7c86dba984a6ea282d9c89793d
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2018-02-02 10:49:22 -0800

    Add tracking for perfect scores for a custom message in toast. Cleanup code

--- app/src/main/java/com/example/android/beerquiz/MainActivity.java
+++ app/src/main/java/com/example/android/beerquiz/MainActivity.java
@@ -19,14 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void gradeSubmission(View view) {
 //initialize variables for answer views
-        RadioButton question1_RadioButton1 = findViewById(R.id.answer_1a);
         RadioButton question1_RadioButton2 = findViewById(R.id.answer_1b);
         CheckBox question2_CheckBox1 = findViewById(R.id.answer_2a);
         CheckBox question2_CheckBox2 = findViewById(R.id.answer_2b);
         CheckBox question2_CheckBox3 = findViewById(R.id.answer_2c);
         RadioButton question3_RadioButton1 = findViewById(R.id.answer_3a);
-        RadioButton question3_RadioButton2 = findViewById(R.id.answer_3b);
-        RadioButton question3_RadioButton3 = findViewById(R.id.answer_3c);
         CheckBox question4_CheckBox1 = findViewById(R.id.answer_4a);
         CheckBox question4_CheckBox2 = findViewById(R.id.answer_4b);
         CheckBox question4_CheckBox3 = findViewById(R.id.answer_4c);
@@ -37,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
         boolean yesQuestion2_CheckBox2 = question2_CheckBox2.isChecked();
         boolean yesQuestion2_CheckBox3 = question2_CheckBox3.isChecked();
         boolean yesQuestion3_RadioButton1 = question3_RadioButton1.isChecked();
-        boolean yesQuestion3_RadioButton2 = question3_RadioButton2.isChecked();
-        boolean yesQuestion3_RadioButton3 = question3_RadioButton3.isChecked();
         boolean yesQuestion4_CheckBox1 = question4_CheckBox1.isChecked();
         boolean yesQuestion4_CheckBox2 = question4_CheckBox2.isChecked();
         boolean yesQuestion4_CheckBox3 = question4_CheckBox3.isChecked();
@@ -47,35 +42,43 @@ public class MainActivity extends AppCompatActivity {
         EditText answerQuestion5 = findViewById(R.id.answer_5);
         EditText answerQuestion6 = findViewById(R.id.answer_6);
 
+//initialize variables
+        int finalGrade = 0;
+        boolean perfectScore = true;
+        String perfectMessage = "";
 
 //now grant score based on which value is true
-        int finalGrade = 0;
-
         if (yesQuestion1_RadioButton2) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
-        if(yesQuestion2_CheckBox1 && yesQuestion2_CheckBox2 && yesQuestion2_CheckBox3) {
+        if (yesQuestion2_CheckBox1 && yesQuestion2_CheckBox2 && yesQuestion2_CheckBox3) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
         if (yesQuestion3_RadioButton1) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
-        if(!yesQuestion4_CheckBox1 && yesQuestion4_CheckBox2 && !yesQuestion4_CheckBox3) {
+        if (yesQuestion4_CheckBox1 && yesQuestion4_CheckBox2 && !yesQuestion4_CheckBox3) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
         if (answerQuestion5.getText().toString().equalsIgnoreCase("growler")) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
         if (answerQuestion6.getText().toString().equalsIgnoreCase("yeast")) {
             finalGrade += 1;
-        }
+        } else perfectScore = false;
 
-        Toast.makeText(getApplicationContext(), "You scored a " + finalGrade + " out of 6", Toast.LENGTH_LONG).show();
+//create custom message based on perfect score or not
+        if (perfectScore) {
+            perfectMessage = ", perfect!";
+        } else perfectMessage = ", study up!";
+
+//Display toast with score and message
+        Toast.makeText(getApplicationContext(), "You scored a " + finalGrade + " out of 6" + perfectMessage, Toast.LENGTH_LONG).show();
     }
 
 }
